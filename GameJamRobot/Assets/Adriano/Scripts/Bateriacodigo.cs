c88b0c018dbb0384ad3322fe13452e6038220d02
Yomoriii <146376934+Yomoriii@users.noreply.github.com>
2024-08-06 15:19:59 -0300

    local de inst da bateria

--- GameJamRobot/Assets/Adriano/Scripts/Bateriacodigo.cs
+++ GameJamRobot/Assets/Adriano/Scripts/Bateriacodigo.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class Bateriacodigo : MonoBehaviour
 {
-    Player player;
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<Player>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        player.playerRB.position = this.transform.position;
+        
     }
 }
