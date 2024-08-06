1c4216eac7559eee4a85c97a964caef031fb0028
Yomoriii <146376934+Yomoriii@users.noreply.github.com>
2024-08-06 16:30:34 -0300

    Volume, comecinho do level design

---
+++ GameJamRobot/Assets/Adriano/Scripts/VolumeChanger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeChanger : MonoBehaviour
+{
+    public Slider slidersom;
+    public TextMeshProUGUI textvolume;
+    public static float volgeral;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volgeral = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = volgeral;
+        textvolume.text = AudioListener.volume.ToString();
+        textvolume.text = $"{AudioListener.volume: 5}";
+    }
+
+    public void somMudar()
+    {
+        volgeral = slidersom.value;
+    }
+}
