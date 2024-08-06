6dfff9913203bf8fcff4dc4136b5bb52acff9263
Pedro Gabriel Vicente <pedrogabrielvicente98@gmail.com>
2024-08-06 14:38:34 -0300

    Terminei Pause e apliquei sprites de botões

--- GameJamRobot/Assets/PedroGabriel/Scripts/PauseMenu.cs
+++ GameJamRobot/Assets/PedroGabriel/Scripts/PauseMenu.cs
@@ -1,30 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject Menu;
     public bool Ativador;
+    string activeScene;
     void Start()
     {
         Menu.SetActive(false);
         Ativador = false;
+        activeScene = SceneManager.GetActiveScene().name;
     }
-
-    
+    public void Resume()
+    {
+        Time.timeScale = 1;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(activeScene);
+        Time.timeScale = 1;
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
     void Update()
     {
         if (Input.GetButtonDown("Cancel") && Ativador == false)
         {
             Menu.SetActive(true);
+            Time.timeScale = 0;
             Ativador = true;
         } else if(Input.GetButtonDown("Cancel") && Ativador == true)
         {
             Menu.SetActive(false);
+            Time.timeScale = 1;
             Ativador = false;
         }
-            
-        
     }
 }
--- GameJamRobot/Assets/PedroGabriel/Scripts/QuitGame.cs
+++ GameJamRobot/Assets/PedroGabriel/Scripts/QuitGame.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class QuitGame : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    
     void Start()
     {
         
-    }
+    }   
 
-    // Update is called once per frame
+    
     void Update()
     {
        
+    }
+    
+    
+    public void CarregarCena()
+    {
+        SceneManager.LoadScene("Game");
     }
     public void SairJogo()
     {
